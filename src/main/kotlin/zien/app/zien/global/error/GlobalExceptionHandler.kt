a8a607faa827aeae7af2749ca2d960807dd1769b
coehgns <modooboiroo@gmail.com>
2025-08-07 16:39:51 +0900

    feat(#3): GlobalExceptionHandler

---
+++ src/main/kotlin/zien/app/zien/global/error/GlobalExceptionHandler.kt
@@ -0,0 +1,32 @@
+package zien.app.zien.global.error
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import zien.app.zien.global.error.exception.ZIENException
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(ZIENException::class)
+    fun handlingZIENException(e: ZIENException): ResponseEntity<ErrorResponse> {
+        val code = e.errorCode
+        return ResponseEntity(
+            ErrorResponse(code.status, code.message),
+            HttpStatus.valueOf(code.status)
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun validatorExceptionHandler(e: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity(
+            ErrorResponse(
+                400,
+                e.bindingResult.allErrors[0].defaultMessage
+            ),
+            HttpStatus.BAD_REQUEST
+        )
+    }
+}
